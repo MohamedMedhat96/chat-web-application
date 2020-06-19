2ebbc6a370f7e67d2288f2093e5ba633e98295bf
MohamedMedhat96 <mohamed.medhat-ali@student.guc.edu.eg>
2020-06-19 21:41:15 +0200

    First commit, adding index.js and index.html

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000 || process.env.PORT
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log("Server is up and running on port " + port)
+})
